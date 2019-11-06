dce3289cae336c3c028672cd3358611a78e45152
Diego Romero <jd@inkatze.com>
2019-11-05 18:09:07 -0800

    Adds working request

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
-const validate = () => {
-  console.debug("test");
-  return "test";
+import { createClientAsync } from "soap";
+
+const url =
+  "https://consultaqr.facturaelectronica.sat.gob.mx/ConsultaCFDIService.svc?wsdl";
+
+const validateCFDI = async (
+  issuerRFC: string,
+  receiverRFC: string,
+  totalAmount: number,
+  invoiceID: string
+) => {
+  const printedExpresion = `re=${issuerRFC}&rr=${receiverRFC}&tt=${totalAmount}&id=${invoiceID}`;
+  const args = {
+    expresionImpresa: printedExpresion
+  };
+
+  let client, response;
+  try {
+    client = await createClientAsync(url);
+  } catch (error) {
+    console.error(error);
+    throw "Error creating a client for the given url";
+  }
+  try {
+    response = await client.ConsultaAsync(args);
+    console.debug(response);
+    return "test";
+  } catch (error) {
+    console.error(error);
+    throw "There was an error while querying with the given parameters";
+  }
 };
 
-export { validate };
+export default validateCFDI;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,11 @@
-import { validate } from "../src";
+import validateCFDI from "../src";
 
-test("Test default return", () => {
-  const result = validate();
+test("Test default return", async () => {
+  const result = await validateCFDI(
+    "LAN8507268IA",
+    "LAN7008173R5",
+    5800.0,
+    "8E4B62EE-3CD3-4874-B75E-F6C0C443A0FB"
+  );
   expect(result).toBe("test");
 });
